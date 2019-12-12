2ba1d65533b8a464b70239f8d9a96ef3c449a945
setala2 <36695312+setala2@users.noreply.github.com>
2019-12-12 19:06:05 +0200

    Made sure that glfw and glew work as intended

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
 #include <iostream>
 
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(1600, 900, "Test window", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glewInit();
+	glClearColor(0, 1, 0, 1);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
