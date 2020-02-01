4d4e704fb702b5775377c60de1bc7fb82170f7ec
setala2 <36695312+setala2@users.noreply.github.com>
2020-02-01 14:28:48 +0200

    Made an improved version of the mesh class shown as an example at https://learnopengl.com/Model-Loading/Mesh. I'll make it use our existing buffer/vertex array classes once I manage to get the model loading to work.

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,6 +1,44 @@
 #include "Mesh.h"
 
+#include <cstddef>
+
 namespace as3d
 {
+	Mesh::Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices, const std::vector<Texture>& textures)
+		: vertices(vertices), indices(indices), textures(textures)
+	{
+		Init();
+	}
 
+	void Mesh::Draw(Shader shader)
+	{
+		// Implement later
+	}
+
+	void Mesh::Init()
+	{
+		// Create the vertex array and the buffers
+		glCheckError(glGenVertexArrays(1, &vao));
+		glCheckError(glGenBuffers(1, &vbo));
+		glCheckError(glGenBuffers(1, &ibo));
+
+		glCheckError(glBindVertexArray(vao));
+		glCheckError(glBindBuffer(GL_ARRAY_BUFFER, vbo));
+
+		// Fill the buffers
+		glCheckError(glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW));
+		glCheckError(glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW));
+
+		// Set up the layout
+		glCheckError(glEnableVertexAttribArray(0));
+		glCheckError(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), NULL));
+
+		glCheckError(glEnableVertexAttribArray(1));
+		glCheckError(glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal)));
+
+		glCheckError(glEnableVertexAttribArray(2));
+		glCheckError(glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords)));
+
+		glCheckError(glBindVertexArray(0));
+	}
 }
--- src/Mesh.h
+++ src/Mesh.h
@@ -1,6 +1,46 @@
 #pragma once
 
+#include "Gldebug.h"
+#include "glm/glm.hpp"
+#include "Shader.h"
+
+#include <vector>
+#include <string>
+
+// My take on the example at https://learnopengl.com/Model-Loading/Mesh
+// I'll likely rewrite this later, and make it use our existing buffer/vertex array classes
+
 namespace as3d
 {
+
+	struct Vertex
+	{
+		glm::vec3 Position;
+		glm::vec3 Normal;
+		glm::vec2 TexCoords;
+	};
+
+	struct Texture
+	{
+		GLuint handle;
+		std::string type;
+		std::string path;
+	};
+
+	class Mesh
+	{
+	public:
+		std::vector<Vertex> vertices;
+		std::vector<unsigned int> indices;
+		std::vector<Texture> textures;
+
+	public:
+		Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices, const std::vector<Texture>& textures);
+		void Draw(Shader shader);
+
+	private:
+		GLuint vao, vbo, ibo;
+		void Init();
+	};
 
 }
