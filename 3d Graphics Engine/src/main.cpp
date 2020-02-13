6dff8bf3b8e65cf9b751f65d59f1e620d8ec4e61
setala2 <36695312+setala2@users.noreply.github.com>
2020-02-13 12:19:00 +0200

    Updated the comments on the main file

--- 3d Graphics Engine/src/main.cpp
+++ 3d Graphics Engine/src/main.cpp
@@ -1,4 +1,4 @@
-#include "GL/glew.h"
+#include <GL/glew.h>
 
 #include "GlfwManager.h"
 #include "ImguiManager.h"
@@ -12,22 +12,25 @@
 #include "Renderer.h"
 #include "Skybox.h"
 
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/matrix_inverse.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
 
-#include "assimp/Importer.hpp"
-#include "assimp/scene.h"
-#include "assimp/postprocess.h"
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 int main()
 {
-	GlfwManager glfwManager;
-
 	constexpr unsigned int windowWidth = 1600;
 	constexpr unsigned int windowHeight = 900;
 	constexpr float aspectRatio = static_cast<float>(windowWidth) / static_cast<float>(windowHeight);
 
+	/////////////////////////////////////
+	//	Set up GLFW, ImGui and GLEW
+	/////////////////////////////////////
+
+	GlfwManager glfwManager;
 	GLFWwindow* window = glfwCreateWindow(1600, 900, "Test window", nullptr, nullptr);
 	glfwMakeContextCurrent(window);
 
@@ -35,18 +38,18 @@ int main()
 
 	glewInit();
 
-	/////////////////////////////
-	//
-	// Testing out Assimp
-	//
-	/////////////////////////////
+	////////////////////////////////////
+	//	Load the models and shaders
+	////////////////////////////////////
 
-	
 	as3d::Model nanoSuit("res/models/nanosuit/nanosuit.obj");
 	as3d::Shader shaderNanoSuit("res/shaders/vertex_assimp.glsl", "res/shaders/fragment_assimp.glsl");
 	as3d::Model light("res/models/cube.obj");
 	as3d::Shader shaderLight("res/shaders/vertex.glsl", "res/shaders/frag.glsl");
-	
+
+	////////////////////////////////////////
+	//	Load the skybox and its shaders
+	////////////////////////////////////////
 
 	std::vector<std::string> skyboxFiles
 	{
@@ -60,6 +63,10 @@ int main()
 	as3d::Skybox skybox(skyboxFiles);
 	as3d::Shader shaderSkybox("res/shaders/skybox_vertex.glsl", "res/shaders/skybox_frag.glsl");
 
+	////////////////////////////////////////////////
+	//	Create the renderer and camera objects
+	////////////////////////////////////////////////
+
 	as3d::Renderer renderer;
 	as3d::Camera camera(glm::perspective(glm::radians(60.0f), aspectRatio, 0.1f, 50.0f));
 
@@ -67,11 +74,13 @@ int main()
 	{
 		renderer.Clear();
 
+		// Render the skybox
 		shaderSkybox.Bind();
 		shaderSkybox.SetMatrix4("projection", camera.GetProjectionMatrix());
 		shaderSkybox.SetMatrix4("view", glm::mat4(glm::mat3(camera.GetViewMatrix())));
 		skybox.Draw(shaderSkybox);
 		
+		// Render the nanosuit model
 		shaderNanoSuit.Bind();
 		shaderNanoSuit.SetMatrix4("projection", camera.GetProjectionMatrix());
 		shaderNanoSuit.SetMatrix4("view", camera.GetViewMatrix());
@@ -80,12 +89,13 @@ int main()
 		shaderNanoSuit.SetVector3("lightPos", light.GetPosition());
 		nanoSuit.Draw(shaderNanoSuit);
 	
+		// Render the light source
 		glm::mat4 lightMVP = camera.GetViewProjectionMatrix() * light.GetModelMatrix();
 		shaderLight.Bind();
 		shaderLight.SetMatrix4("mvp", lightMVP);
 		light.Draw(shaderLight);
 		
-
+		// Render the GUI
 		imgui.BeginFrame();
 		camera.DrawControlWindow("camera");
 		nanoSuit.DrawControlWindow("model");
