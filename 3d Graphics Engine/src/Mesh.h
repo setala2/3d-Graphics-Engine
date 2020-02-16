d315eda690c88586e46845c8efc0b7aa5d8b6e3b
setala2 <36695312+setala2@users.noreply.github.com>
2020-02-16 12:55:15 +0200

    Cleaned up the Mesh.h

--- 3d Graphics Engine/src/Mesh.h
+++ 3d Graphics Engine/src/Mesh.h
@@ -1,37 +1,27 @@
 #pragma once
 
 #include "Gldebug.h"
-#include "glm/glm.hpp"
 #include "Shader.h"
 #include "VertexBuffer.h"
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Texture.h"
 
+#include <glm/glm.hpp>
+
 #include <vector>
 #include <string>
 
 // My take on the example at https://learnopengl.com/Model-Loading/Mesh
-// I'll likely rewrite this later, and make it use our existing buffer/vertex array classes
 
 namespace as3d
 {
-
 	struct Vertex
 	{
 		glm::vec3 Position;
 		glm::vec3 Normal;
 		glm::vec2 TexCoords;
 	};
-
-	/*struct Texture
-	{
-		GLuint handle;
-		std::string type;
-		std::string path;
-
-		void Bind(unsigned int slot) const;
-	};*/
 
 	class Mesh
 	{
